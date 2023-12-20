e52167676818722dfba7ad9cb4b8cdd30fe337c4
Sagar Mathur <145686719+Sagar-Mathur@users.noreply.github.com>
2023-12-20 09:54:30 -0800

    index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import NewRegistration from './NewRegistration';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NewRegistration/>
+    <App/>
   </React.StrictMode>
 );
 
