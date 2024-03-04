8b9207ad489a1a8f8c67c190040884c452b71f40
Sagar Mathur <145686719+Sagar-Mathur@users.noreply.github.com>
2024-03-04 05:06:49 -0700

    NewRegistration.js

--- NewRegistration.js
+++ NewRegistration.js
@@ -1,9 +1,34 @@
 import './Newform.css';
 import Loginpage from './Loginpage';
+import { useState } from 'react';
 function NewRegistration(){
+    const[formdata,setformdata]=useState({
+        username:'',
+        email:'',
+        password:''
+       })
+       const handlechange=  (e) =>{
+        setformdata({ ...formdata, [e.target.name]: e.target.value });          
+       }
+        console.log(formdata)
+       const handlesubmit=async(e)=>{
+        e.preventDefault();
+           try{
+               const response=await fetch("http://localhost:5000/register",{
+                   method: 'POST',
+                   header:{
+                       'Content-Type':'application/json'
+                   },
+                   body:JSON.stringify(formdata)
+                })
+                console.log(formdata)
+                const data= await response.json();
+                console.log(data.message);
+           } catch(error){
+               console.error('ERROR:',error);
+           }
+        }
    
-    let a= Map.text
-     console.log(a)
     function close()
     {
        
@@ -17,28 +42,30 @@ function NewRegistration(){
 		          <span id="close-btn" onClick={close}>X</span>
 		          <h2>New Registration</h2>
                   <img src="" />
-		          <form>
+		          <form onSubmit={handlesubmit}>
                     <label>Name :</label>
-			        <input type="text" name=""  placeholder='Enter Name'/>
-                    <br></br>
+                    <input type="text" name="username" placeholder="Username" onChange={handlechange} />                    <br></br>
                     <br></br>
                     <label>Phone Number :</label>
-                    <input type='Number'  placeholder='phone Number' />
+                    <input type="email" name="email" placeholder="Email" onChange={handlechange} />
                     <br></br>
                     <br></br>
                     <label>Password :</label>
-                    <input type='New-password' placeholder='Password'/>
+                    <input type="password" name="password" placeholder="Password" onChange={handlechange} />
                     <br></br>
                     <br></br>
-                    <label>Confirm Password :</label>
-                    <input type='password' placeholder='Re-Password'/>
-                    <br></br>
-                    <br></br>
+                   
                     <br></br>
 			        <input type="submit" name=""/>
 		</form>
 	</div>
  </div>
+        </div>
+    )
+} 
+
+
+export default NewRegistration;
         </div>
     )
 } 
