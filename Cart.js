60e62ba735491cf57589327257ce8e23aaf70288
Sagar Mathur <145686719+Sagar-Mathur@users.noreply.github.com>
2024-03-04 04:59:10 -0700

    Cart.js

--- Cart.js
+++ Cart.js
@@ -1,16 +1,55 @@
 // Cart.js
 import React from 'react';
 import { connect } from 'react-redux';
+import Product from './Product';
+import { useState } from 'react';
+import './cart.css';
+import store from './store';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart}) => {
+  function hide(){
+    document.getElementById('show').style.display='none';
+  }
+  function product(){
+    document.getElementById('addtocart-popup').style.display='none';
+
+  }
+
+  
+  
   return (
     <div>
-      <h2>Shopping Cart</h2>
-      <ul>
-        {cart.map((item, index) => (
-          <li key={index}>{item}</li>
+      <h2></h2>
+      <table className='product-table' id='show'> 
+     
+          <tr>
+            <th>product</th>
+            <th>quantity</th>
+            <th>price</th>
+          </tr>
+            <button id='minimize-btn' onClick={hide}>x</button>
+        {cart.map((item) => (
+          <tr>
+            <td><img id='shop-image' src={item.img} /><span className='product-name'>{item.name}</span></td>
+            <td> <span className='product-quantity'><input type='number'  id='input-tag'/></span></td>
+            <td ><span className='product-shop-price'>{item.price}</span></td>
+          </tr>
+
         ))}
-      </ul>
+      </table>
+      <h2 id='item'>{cart.length}</h2>
+        {cart.map((item)=>(
+      <div className='addtocart-popup' id='addtocart-popup'>
+        <button onClick={product} >x</button>
+          <div>
+          <img src={item.img}/>
+          <span>{item.name}</span>
+          <h2>{item.price}</h2>
+          </div>
+      </div>
+        ))}
+
+
     </div>
   );
 };
